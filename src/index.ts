0cafd877058779a2554cc6981486fae4827b34c8
Ladislav Gazo <gazo@seges.sk>
2023-11-06 14:13:44 +0100

    replaced pg-promise with postgres library

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-import pgPromise from "pg-promise";
-import type { IClient } from "pg-promise/typescript/pg-subset";
+import postgres from 'postgres';
 
-// Initialize pg-promise and connect to your database
-const pgp = pgPromise();
+export type Db = postgres.Sql<{}>;
 
-export type Db = pgPromise.IDatabase<{}, IClient>;
 export type DatabaseUrl = string;
 export type DbConfig = {
     databaseUrl: DatabaseUrl
@@ -14,33 +11,33 @@ export type DbConfig = {
 export const acquireLock = (db: Db) => async (
     streamName: string
 ): Promise<number> => {
-    return db.one(
-        `SELECT message_store.acquire_lock($1)`,
-        [streamName],
-        (a: { acquire_lock: number }) => a.acquire_lock
-    );
+    type Result = {
+        acquire_lock: number
+    };
+    const [ result ] = await db<Result[]>`SELECT message_store.acquire_lock(${streamName})`;
+    return result.acquire_lock;
 };
 
 // Function that wraps around the cardinal_id stored procedure
 export const getCardinalId = (db: Db) => async (
     streamName: string
 ): Promise<string | null> => {
-    return db.oneOrNone(
-        `SELECT message_store.cardinal_id($1)`,
-        [streamName],
-        (a: { cardinal_id: string | null }) => a.cardinal_id
-    );
+    type Result = {
+        cardinal_id: string
+    };
+    const [ result ] = await db<Result[]>`SELECT message_store.cardinal_id(${streamName})`;
+    return result.cardinal_id;
 };
 
 // Function that wraps around the category stored procedure
 export const getCategory = (db: Db) => async (
     streamName: string
 ): Promise<string> => {
-    return db.one(
-        `SELECT message_store.category($1)`,
-        [streamName],
-        (a: { category: string }) => a.category
-    );
+    type Result = {
+        category: string
+    };
+    const [ result ] = await db<Result[]>`SELECT message_store.category(${streamName})`;
+    return result.category;
 };
 
 // Function that wraps around the get_category_messages stored procedure
@@ -53,10 +50,8 @@ export const getCategoryMessages = (db: Db) => async (
     consumerGroupSize: number | null = null,
     condition: string | null = null
 ): Promise<any> => {
-    return db.any(
-        `SELECT * FROM message_store.get_category_messages($1, $2, $3, $4, $5, $6, $7)`,
-        [category, position, batchSize, correlation, consumerGroupMember, consumerGroupSize, condition]
-    );
+    const result = await db`SELECT * FROM message_store.get_category_messages(${category}, ${position}, ${batchSize}, ${correlation}, ${consumerGroupMember}, ${consumerGroupSize}, ${condition})`;
+    return result;
 };
 
 // Function that wraps around the get_last_stream_message stored procedure
@@ -64,10 +59,8 @@ export const getLastStreamMessage = (db: Db) => async (
     streamName: string,
     type: string | null = null
 ): Promise<any> => {
-    return db.any(
-        `SELECT * FROM message_store.get_last_stream_message($1, $2)`,
-        [streamName, type]
-    );
+    const result = await db`SELECT * FROM message_store.get_last_stream_message(${streamName}, ${type})`;
+    return result;
 };
 
 // Function that wraps around the get_stream_messages stored procedure
@@ -77,52 +70,50 @@ export const getStreamMessages = (db: Db) => async (
     batchSize: number = 1000,
     condition: string | null = null
 ): Promise<any> => {
-    return db.any(
-        `SELECT * FROM message_store.get_stream_messages($1, $2, $3, $4)`,
-        [streamName, position, batchSize, condition]
-    );
+    const result = await db`SELECT * FROM message_store.get_stream_messages(${streamName}, ${position}, ${batchSize}, ${condition})`;
+    return result;
 };
 
 // Function that wraps around the hash_64 stored procedure
 export const getHash64 = (db: Db) => async (
     value: string
 ): Promise<number> => {
-    return db.one(
-        `SELECT message_store.hash_64($1)`,
-        [value],
-        (a: { hash_64: number }) => a.hash_64
-    );
+    type Result = {
+        hash_64: number
+    };
+    const [ result ] = await db<Result[]>`SELECT message_store.hash_64(${value})`;
+    return result.hash_64;
 };
 
 // Function that wraps around the id stored procedure
 export const getId = (db: Db) => async (
     streamName: string
 ): Promise<string | null> => {
-    return db.oneOrNone(
-        `SELECT message_store.id($1)`,
-        [streamName],
-        (a: { id: string | null }) => a.id
-    );
+    type Result = {
+        id: string | null
+    };
+    const [ result ] = await db<Result[]>`SELECT message_store.id(${streamName})`;
+    return result.id;
 };
 
 // Function that wraps around the message_store_version stored procedure
 export const getMessageStoreVersion = (db: Db) => async (): Promise<string> => {
-    return db.one(
-        `SELECT message_store.message_store_version()`,
-        [],
-        (a: { message_store_version: string }) => a.message_store_version
-    );
+    type Result = {
+        message_store_version:string
+    };
+    const [ result ] = await db<Result[]>`SELECT message_store.message_store_version()`;
+    return result.message_store_version;
 };
 
 // Function that wraps around the stream_version stored procedure
 export const getStreamVersion = (db: Db) => async (
     streamName: string
 ): Promise<number> => {
-    return db.one(
-        `SELECT message_store.stream_version($1)`,
-        [streamName],
-        (a: { stream_version: number }) => a.stream_version
-    );
+    type Result = {
+        stream_version: number
+    };
+    const [ result ] = await db<Result[]>`SELECT message_store.stream_version(${streamName})`;
+    return result.stream_version;
 };
 
 export const writeMessage = (db: Db) => async (
@@ -133,17 +124,20 @@ export const writeMessage = (db: Db) => async (
     metadata: object | null = null,
     expectedVersion: number | null = null
 ): Promise<number> => {
-    return db.one(
-        `SELECT message_store.write_message($1, $2, $3, $4, $5, $6)`,
-        [id, streamName, type, data, metadata, expectedVersion],
-        (a: { write_message: number }) => a.write_message
-    );
+    type Result = {
+        write_message: number
+    };
+    const [ result ] = await db<Result[]>`SELECT message_store.write_message(${id}, ${streamName}, ${type}, ${JSON.stringify(data)}, ${JSON.stringify(metadata)}, ${expectedVersion})`;
+    return result.write_message;
 };
 
 export const create = (config: DbConfig) => {
     console.debug(`starting ...`, config);
-    // const db = pgp(DATABASE_URL);
-    const db = pgp(config.databaseUrl);
+    const db: postgres.Sql<{}> = postgres(config.databaseUrl, {
+        connection: {
+            search_path: 'message_store,public'
+        }
+    });
     console.debug(`connected ...`, config);
 
     return {
@@ -156,7 +150,7 @@ export const create = (config: DbConfig) => {
         getHash64: getHash64(db),
         getId: getId(db),
         getMessageStoreVersion: getMessageStoreVersion(db),
-        getStreamVersion: getStreamMessages(db),
+        getStreamVersion: getStreamVersion(db),
         writeMessage: writeMessage(db)
     }
 };
